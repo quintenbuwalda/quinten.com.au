4537f283127f19b73fbff0c32a0788bdcbbb7f50
Quinten Buwalda <quintenbuwalda@gmail.com>
2024-05-15 11:54:29 +1000

    added solutions

---
+++ solutions/scripts.js
@@ -0,0 +1,3 @@
+document.getElementById('book-consultation').addEventListener('click', function() {
+    window.location.href = 'mailto:consultation@example.com?subject=Book%20a%20Consultation';
+});
