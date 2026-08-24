8dd1b9ed0fcd1b981b6424e46841e828966097d9
Quinten <quintenbuwalda@gmail.com>
2026-08-24 18:27:40 +1000

    we're going sicko mode

--- build.sh
+++ build.sh
@@ -16,7 +16,7 @@ if ! command -v pdflatex >/dev/null || ! command -v lwarpmk >/dev/null; then
     export PATH="$HOME/.TinyTeX/bin/x86_64-linux:$PATH"
 
     tlmgr install \
-      scheme-medium \
+      scheme-full \
       lwarp \
       cm-super
 fi
